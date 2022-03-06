fc9f5b0560a2a87985df6ad343fa6fc7d22b26fd
Mansur <mansurkhan55555@gmail.com>
2022-03-06 15:28:55 +0530

    Login and data fetching from database

--- app/src/main/java/com/anuraagpotdaar/unitrials/ProfileAndSettings.java
+++ app/src/main/java/com/anuraagpotdaar/unitrials/ProfileAndSettings.java
@@ -2,13 +2,43 @@ package com.anuraagpotdaar.unitrials;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileAndSettings extends AppCompatActivity {
+
+    TextView btn_back;
+    Button btn_logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_and_settings);
+
+        btn_back = findViewById(R.id.txt_btn_back);
+        btn_logOut = findViewById(R.id.btn_logout);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
