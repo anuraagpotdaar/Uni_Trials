2e4ef5a10489c876273012f0e3d48c9c2253e19c
Anuraag <>
2022-03-10 06:36:58 +0530

    Add meds implemented.

--- app/src/main/java/com/anuraagpotdaar/unitrials/AddMedicationFragment.java
+++ app/src/main/java/com/anuraagpotdaar/unitrials/AddMedicationFragment.java
@@ -2,25 +2,49 @@ package com.anuraagpotdaar.unitrials;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.anuraagpotdaar.unitrials.databinding.FragmentAddMedicationBinding;
+import com.anuraagpotdaar.unitrials.databinding.FragmentMedicationBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddMedicationFragment extends BottomSheetDialogFragment {
 
+    private FragmentAddMedicationBinding binding;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAddMedicationBinding.inflate(inflater, container, false);
+
+        String selected = getActivity().getIntent().getStringExtra("selected participant");
+
+        DatabaseReference medsReference = FirebaseDatabase.getInstance().getReference("Patient List/"+selected+"/meds");
+
+        binding.btnMedDone.setOnClickListener(
+                view -> {
+                    medsReference.child(binding.etMedName.getText().toString()).child("Morning").setValue(binding.etMorning.getText().toString());
+                    medsReference.child(binding.etMedName.getText().toString()).child("Afternoon").setValue(binding.etAfternoon.getText().toString());
+                    medsReference.child(binding.etMedName.getText().toString()).child("Evening").setValue(binding.etEvening.getText().toString());
+                    medsReference.child(binding.etMedName.getText().toString()).child("Night").setValue(binding.etNight.getText().toString());
+                    dismiss();
+                });
+        return binding.getRoot();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_medication, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
